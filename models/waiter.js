ce0084182c949b3ed62f5a5721ccab24daca4e59
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-09-01 08:49:39 -0400

    Fix TravisCI error for waiter.js

--- models/waiter.js
+++ models/waiter.js
@@ -1,12 +1,12 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   // Creates a "Waiter" model that matches up with DB
   const Waiter = sequelize.define("Waiter", {
     name: DataTypes.STRING,
     rating: DataTypes.STRING,
-    tipAmount: DataTypes.DECIMAL,
+    tipAmount: DataTypes.DECIMAL
   });
 
-  Waiter.associate = function (models) {
+  Waiter.associate = function(models) {
     Waiter.belongsTo(models.Restuarant, {
       foreignKey: {
         allowNull: false
